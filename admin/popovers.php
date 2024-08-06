5d2e796c274ebf2d5ec1fa9b28d62899ad603418
llenny18 <ramosalleneid01@gmail.com>
2024-08-06 22:54:19 +0800

    footer

--- admin/popovers.php
+++ admin/popovers.php
@@ -132,14 +132,7 @@
       </div>
 
       <!-- Footer -->
-      <footer class="sticky-footer bg-white">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>copyright &copy; <script> document.write(new Date().getFullYear()); </script> - developed by <b><a href="https://indrijunanda.gitlab.io/"
-                  target="_blank">indrijunanda</a></b> </span>
-          </div>
-        </div>
-      </footer>
+      < <?php include("./footer.php"); ?>
     </div>
   </div>
   <a class="scroll-to-top rounded" href="#page-top">
