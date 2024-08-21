1817eb217734f7cfa1f9c55e284e3cd0d1099eda
llenny18 <ramosalleneid01@gmail.com>
2024-08-21 07:57:52 -0700

    dashboard herherer

--- model/conn.php
+++ model/conn.php
@@ -18,4 +18,128 @@ if ($conn->connect_error) {
 // Close the connection
 // Uncomment this line to close the connection after operations
 // $conn->close();
+
+
+function checkColor($num){
+    if($num<0){
+        echo "danger";
+    }
+    else {
+        echo "success";
+    }
+
+}
+
+
+function checkArrow($num){
+    if($num<0){
+        echo "down";
+    }
+    else {
+        echo "up";
+    }
+
+}
+function checkText($num){
+    if($num<0){
+        echo " Less";
+    }
+    else {
+        echo " More";
+    }
+
+}
+
+function countQR($conn) {
+
+    $sql = "SELECT count(*) as count FROM `qr_codes` WHERE 1;";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+$row = $result->fetch_assoc();
+
+return $row['count'];
+
+}
+}
+
+function countQRM($conn) {
+
+    $sql ='SELECT * FROM `qrm`';
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+$row = $result->fetch_assoc();
+
+return $row['count'];
+}
+}
+
+
+function countPatientM($conn) {
+
+    $sql ='SELECT * FROM `pm`';
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+$row = $result->fetch_assoc();
+
+return $row['patient_checkup_count_difference'];
+}
+}
+
+
+
+function countWorkerM($conn) {
+
+    $sql ='SELECT * FROM `wm`';
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+$row = $result->fetch_assoc();
+
+return $row['health_worker_created_difference'];
+}
+}
+
+
+function countRecordM($conn) {
+
+    $sql ='SELECT * FROM `rm`';
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+$row = $result->fetch_assoc();
+
+return $row['checkup_count_difference'];
+}
+}
+function countPatient($conn) {
+
+    $sql = "SELECT count(*) as count FROM `patients` WHERE 1;";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+$row = $result->fetch_assoc();
+
+return $row['count'];
+}
+}
+function countWorker($conn) {
+
+    $sql = 'SELECT count(*) as count FROM `users` WHERE role = "health_worker"';
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+$row = $result->fetch_assoc();
+
+return $row['count'];
+}
+}
+
+function countRecords($conn) {
+
+    $sql = 'SELECT count(*) as count FROM `medical_records` WHERE 1';
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+$row = $result->fetch_assoc();
+
+return $row['count'];
+}
+}
+
+
 ?>
